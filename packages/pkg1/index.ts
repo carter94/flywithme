801932c2becbc8dad9f4d2d2bb55c54d8c390666
carter111 <673164908@qq.com>
2024-04-27 20:12:52 +0800

    测试

---
+++ packages/pkg1/index.ts
@@ -0,0 +1,8 @@
+import func2 from '@cfz/monorepo2';
+
+function func1() {
+    func2();
+    console.log('im pkg1');
+}
+
+export default func1;
---
+++ packages/pkg2/index.js
@@ -0,0 +1,5 @@
+function func1() {
+    console.log('im pkg2');
+}
+
+export default func1;
