f56df37a3b17864614076afc6fa08d809ad5d355
carter111 <673164908@qq.com>
2024-04-27 22:36:44 +0800

    feat: 123

--- packages/pkg1/index.ts
+++
@@ -1,8 +0,0 @@
-import func2 from '@cfz/monorepo2';
-
-function func1() {
-    func2();
-    console.log('im pkg1');
-}
-
-export default func1;
--- packages/pkg2/index.js
+++
@@ -1,5 +0,0 @@
-function func1() {
-    console.log('im pkg2');
-}
-
-export default func1;
